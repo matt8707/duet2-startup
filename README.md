1b534b4af3f9b3ef9c4f3055e8063942e6a4f851
Mattias Persson <mattias.persson.87@gmail.com>
2022-09-07 16:24:26 +0200

    update readme

--- README.md
+++ README.md
@@ -5,14 +5,12 @@
 ```py
 duet_2(uid["input_type"], chl["1"], cmd["Inst"])
 duet_2(uid["input_type"], chl["2"], cmd["Inst"])
-```
-
-```py
-# initial output gain can be set here instead
-# /Library/Application Support/Apogee/Settings/DuetII8300000000002C71.xml
 
 # duet_2(uid["output_level"], chl["1"], vol_out(-55))
 # duet_2(uid["output_level"], chl["2"], vol_out(-55))
+
+# initial output gain can be set here instead
+# /Library/Application Support/Apogee/Settings/DuetII8300000000002C71.xml
 ```
 
 Thanks to https://github.com/stefanocoding/take_control
--- main.py
+++ main.py
@@ -82,10 +82,14 @@ cmd = {
 
 
 try:
-    # duet_2(uid["output_level"], chl["1"], vol_out(-55))
-    # duet_2(uid["output_level"], chl["2"], vol_out(-55))
     duet_2(uid["input_type"], chl["1"], cmd["Inst"])
     duet_2(uid["input_type"], chl["2"], cmd["Inst"])
+
+    # duet_2(uid["output_level"], chl["1"], vol_out(-55))
+    # duet_2(uid["output_level"], chl["2"], vol_out(-55))
+
+    # initial output gain can be set here instead
+    # /Library/Application Support/Apogee/Settings/DuetII8300000000002C71.xml
 
 except AttributeError:
     print("Apogee Duet 2 not found...")
